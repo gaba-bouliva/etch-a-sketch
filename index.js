36e0250014479fd4111c788b4bbc28a3b7b1de82
Gaba Bouliva <eaglebald111@gmail.com>
2023-01-24 01:13:25 +0100

    Create a webpage with a 16x16 grid of square divs

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Js link works!");
+const sketchBoard = document.querySelector('.sketch-board');
+const sketchBoardHeight = getComputedStyle(sketchBoard).height;
+const sketchBoardWidth = getComputedStyle(sketchBoard).width;
+
+let nbrCells = 16 ;
+
+for (let row = 1; row <= nbrCells; row++) {
+  const rowDiv = document.createElement('div');
+  
+  for (let columns = 1; columns <= nbrCells; columns++) {
+    
+    const colDiv = document.createElement('div');
+    colDiv.style.border = '1px solid black';
+    colDiv.style.height = `${parseFloat(sketchBoardHeight) / nbrCells}px`;
+    colDiv.style.width = `${parseFloat(sketchBoardWidth) / nbrCells}px `;
+    rowDiv.style.height = colDiv.style.height;
+    rowDiv.appendChild(colDiv)
+
+    sketchBoard.appendChild(rowDiv)
+  }
+}
